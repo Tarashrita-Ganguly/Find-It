076201ae56ebe848631b3f164198f51a5509ec8d
Tarashrita Ganguly <150145692+Tarashrita-Ganguly@users.noreply.github.com>
2024-01-22 20:25:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 video="";
+status="";
+objects=[];
 
 function preload(){
-    
-   
 }
 
 function setup() {
@@ -12,9 +12,6 @@ function setup() {
     video.hide();
 }
 
-function draw(){
-    image(video,0,0,480,380);
-}
 function start() {
     document.getElementById("status").innerHTML = "Status: Detecting objects";
     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
@@ -24,4 +21,30 @@ function modelLoaded(){
     console.log("model loaded!");
     status=true;
    
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        objects=results
+    };
+}
+
+function draw(){
+    image(video,0,0,480,380);
+    if(status !=""){
+        objectDetector.detect(video, gotResult);
+        for(i=0; i<objects.length; i++){
+           document.getElementById("status").innerHTML="Status:Found objects";
+           document.getElementById("number_of_objects").innerHTML="Number of objects detected are"+objects.length;
+           fill("#ff0000");
+           percentage=floor(objects[i].confidence * 100);
+           text(objects[i].label+""+percentage+"%", objects[i].x+15, objects[i].y+15);
+           noFill();
+           stroke("#ff0000");
+           rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    };
 }
